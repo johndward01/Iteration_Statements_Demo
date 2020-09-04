77643c29716fad38d6129784e6b0a2eaaa9ecc6b
johnd <johndward01@yahoo.com>
2020-09-04 14:47:04 -0500

    fixed index out of range exception on the loop that prints out only the even numbers

--- Iteration_Statements_Demo/Program.cs
+++ Iteration_Statements_Demo/Program.cs
@@ -10,18 +10,31 @@ namespace Iteration_Statements_Demo
             var list = new List<int>() { 1, 2, 3 }; //An example List<int> (a list of integers) to play around with using Loops
 
             //for loop example ---> type (for) and hit Tab twice
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
+
+            //another for loop example using the list above
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i]);
             }
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //foreach loop example ---> type (foreach) and hit Tab twice
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //while loop example ---> type (while) and hit Tab twice
             var index1 = 0;
@@ -30,7 +43,9 @@ namespace Iteration_Statements_Demo
                 Console.WriteLine(list[index1]);
                 index1++;
             }
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //do-while loop example ---> type (do) and hit Tab twice
             var index2 = 0;
@@ -39,7 +54,9 @@ namespace Iteration_Statements_Demo
                 Console.WriteLine(list[index2]);
                 index2++;
             } while (index2 < list.Count);
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //--------------------------------------------------------------------------------------------------------------------------------
 
@@ -50,14 +67,21 @@ namespace Iteration_Statements_Demo
             {
                 Console.WriteLine(number);
             }
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
-
-            //TODO --> Use a for loop to print only the EVEN numbers
-            for (int i = 1; i < arr.Length; i+=2)
+            //TODO --> Use a for loop to print only the EVEN numbers            
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(arr[i]);
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }                
             }
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //TODO --> Using a while loop, print out to the console "This is not the number you're looking for" 10 times
 
@@ -69,7 +93,9 @@ namespace Iteration_Statements_Demo
                 Console.WriteLine("This is not the number you're looking for");
                 x++; //we need to increment x otherwise we will have an infinite loop because the condition (x < 10) will always be true
             }
-
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
 
             //TODO --> Using a do-while loop, ask the user for their favorite number then print out that number 10 times
 
@@ -84,6 +110,9 @@ namespace Iteration_Statements_Demo
                 Console.WriteLine(favoriteNumber);
                 y++;
             } while (y < 10);
+            Console.WriteLine();
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
         }
     }
 }
